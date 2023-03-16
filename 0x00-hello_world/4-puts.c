c892d3f6db441da84b99984c815e1b9ef9c2d65a
hamza <you@example.com>
2023-03-16 11:10:26 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
-/**
- *main - Entry point
 
-Returne: Always 0 (Sacceess)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char hello[] = "\"Programming is like building a multilingual puzzle\n";
