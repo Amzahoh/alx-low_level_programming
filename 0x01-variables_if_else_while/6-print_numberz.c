23ba2092868107c592ad541de58e6e441769c3e1
hamza <you@example.com>
2023-03-17 14:44:07 -0700

    hjh

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * test
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+	putchar("%d", i);
+	i++;
+	}
+	putchar("\n");
+	return (0);
