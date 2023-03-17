9a574f7077b40f456aeb608648743c5afabc8778
hamza <you@example.com>
2023-03-17 11:52:51 -0700

    1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char alphi = 'a';
+
+        while (alpha <= 'z')
+        {
+                putchar(alph);
+                alph++;
+        }
+                putchar('\n');
+        return (0);
+}
