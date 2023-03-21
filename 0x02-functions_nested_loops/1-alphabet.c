9cdd563c6e089b253ad5cfedfb5d9714ee1516d5
hamza <you@example.com>
2023-03-21 12:04:28 -0700

    jh

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+		_putchar('\n');
+		return (0);
+}
