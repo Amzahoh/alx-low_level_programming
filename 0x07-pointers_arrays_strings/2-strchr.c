0d6f996d8103de7e4ee6a94150fd2b97707eed91
hamza <you@example.com>
2023-04-03 09:26:55 -0700

    task 2 that search for the fisrt occurence of a given character on a given string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,12 +12,12 @@ char *_strchr(char *s, char c)
 
 	while (s[i])
 	{
-	if (s[i] == c)
-	{
-	return (s + i);
+		if (s[i] == c)
+		{
+		return (&s[i]);
+		}
+			i++;
 	}
-	i++;
-	}
-	return (NULL);
+		return (NULL);
 }
 
