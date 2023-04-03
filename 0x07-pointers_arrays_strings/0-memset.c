43723bd930b380d1a8a08915cd8ac12200482037
hamza <you@example.com>
2023-04-03 07:31:28 -0700

    changing wile loop with for filling the memory with a set constant

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,17 +5,16 @@
  * @s: is a pointer
  * @b: is a chaaracter to set
  * @n: the number of byte to fill
- * return: a pointer to the memory area s
+ * Return: the pointer s to the memory area.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
-	while (i < n)
+	for (i = 0; n > 0; i++)
 	{
 		s[i] = b;
-		i++;
+		n--;
 	}
 	return (s);
 }
-
