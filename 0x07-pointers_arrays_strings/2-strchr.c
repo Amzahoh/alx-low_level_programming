ddf2e1f816032356003de7d9a2acc1eca67dfa83
hamza <you@example.com>
2023-04-03 09:06:52 -0700

    task 2 that search for the fisrt occurence of a given character on a given string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * _strchr - shearch for the occurance
+ * of a given character on a string
+ *@s: is a pointer
+ *@c: character to search
+ * Return: thepointer s or null if unfound
+i */
+char *_strchr(char *s, char c)
+{
+	int i = 0, len;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	len = i;
+
+	for (i = 0; i < len ; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+		return (NULL);
+}
