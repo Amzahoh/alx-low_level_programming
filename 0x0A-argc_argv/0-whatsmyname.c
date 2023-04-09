3f9ac77a0f44cd62b5a7028c562c67d3fa245ff8
hamza <you@example.com>
2023-04-09 06:58:51 -0700

    fixing errors

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,16 +2,17 @@
 #include "main.h"
 /**
  * main - entry point
- *
+ * @argc: count the number of argment
+ * @argv: array of argument
  * Return: On success 0.
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	if (argc > 0)
 	{
-		for (i= 0; argv[0][i] != '\0'; i++)
+		for (i = 0; argv[0][i] != '\0'; i++)
 		{
 			_putchar(argv[0][i]);
 		}
