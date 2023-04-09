22cae423949082a3b5c24cb2057d0dd35ee32f98
hamza <you@example.com>
2023-04-09 08:37:25 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: count the number of argment
+ * @argv: array of argument
+ * Return: On success 0.
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	_putchar(argc + '0');
+	_putchar('\n');
+	return (0);
+}
