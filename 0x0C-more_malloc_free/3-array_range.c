4e40cafa5df5f1bd9f46dc3b556cfbba60156715
hamza <you@example.com>
2023-04-13 14:13:45 -0700

    task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+/**
+ * * array_range - a function that order an array
+ * @max: interger
+ * @min: integer
+ * Return: a pointer to the allocated memory or exit.
+ */
+int *array_range(int min, int max)
+{
+	int *arr;
+	int i, len;
+
+	if (min > max)
+		return (NULL);
+	len = max - min + 1;
+	arr = malloc(len * sizeof(int));
+
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		arr[i] = min + i;
+	return (arr);
+}
