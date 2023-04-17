08cc2814a0c86e9d9f0d940ade4d5ab0c2dc5a6b
hamza <you@example.com>
2023-04-17 16:11:07 -0700

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include<stddef.h>
+/**
+ * print_dog - function that initiliase
+ * @d: pointer to the adress of the structure
+ * Return : If an element of d is NULL, print (nil) instead of this element.
+ * (if name is NULL, print Name: (nil))
+ * If d is NULL print nothing.
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (!d->name)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+
+		printf("Age: %f\n", d->age);
+
+		if (!d->owner)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
