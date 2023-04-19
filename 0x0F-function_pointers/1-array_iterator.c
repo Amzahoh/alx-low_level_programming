89a584286641e9ec69c4454723ab1bfb43cf6af8
hamza <you@example.com>
2023-04-19 13:51:45 -0700

    fixing the output error oftask 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * array_iterator - entry point of the function
  * @array: yhe array
@@ -11,6 +12,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i <= size; i++)
+	if (array == NULL || action == NULL)
+		exit(2);
+	for (i = 0; i < size; i++)
+	{
 	action(array[i]);
 	}
+}
