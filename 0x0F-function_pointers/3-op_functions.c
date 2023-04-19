ed72efc294bca2ef21818f048e982a01d1b89c3d
hamza <you@example.com>
2023-04-19 15:32:30 -0700

    crating the op_function  and its functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * op_add - addition
+ * @a: int a
+ * @b: int b
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts
+ * @a: int a
+ * @b: int b
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication
+ * @a: int a
+ * @b: int b
+ * Return: multiply
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @a: int a
+ * @b: int b
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulos
+ * @a: int a
+ * @b: int b
+ * Return: modulo of ints
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
